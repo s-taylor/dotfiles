d08bcab5591bd08a9b38a50e106d18310c81c240
Simon Taylor <nizmox@gmail.com>
2015-09-10 16:42:13 +1000

    persist current path on new panes and windows

--- reinstall.sh
+++ reinstall.sh
@@ -11,8 +11,9 @@ cp .gitconfig ~/
 #update bashprofile
 rm ~/.bash_profile
 cp .bash_profile ~/
-source ~/.bash_profile
+source ~/.bash_profile #reload bash_profile
 
 #update gitconfig
 rm ~/.tmux.conf
 cp .tmux.conf ~/
+tmux source-file ~/.tmux.conf #reload tmux config
