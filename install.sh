aa3f32033083a3b52a0171d1fa6ab604e912d912
Simon Taylor <nizmox@gmail.com>
2016-05-02 17:18:17 +1000

    add nvm install to install shell script

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 #install silver searcher (Ag)
 brew install the_silver_searcher
+
+#install node version manager (nvm)
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
